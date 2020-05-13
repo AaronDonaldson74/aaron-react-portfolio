ee851445f93509b25f3e58e77e086266c6b2ad31
AaronDonaldson74 <Dev.Aaron.Donaldson@gmail.com>
2020-05-13 16:23:09 -0600

    React Constructor Overview

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -2,6 +2,11 @@ import React, { Component } from "react";
 import PortfolioItem from "./portfolio-item";
 
 export default class PortfolioContainer extends Component {
+    constructor() {
+        super();
+        console.log("Portfolio container has rendered");
+    }
+
     render() {
         return (
             <div>
