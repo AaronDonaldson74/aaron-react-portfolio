c335785d513807b975a6dda873e1099eb847a074
AaronDonaldson74 <Dev.Aaron.Donaldson@gmail.com>
2020-05-13 16:50:45 -0600

    Overview of Props in React

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -11,7 +11,7 @@ export default class PortfolioContainer extends Component {
         const data = ["Quip", "Eventbright", "Ministry Safe"];
 
         return data.map(item => {
-            return <PortfolioItem />;
+            return <PortfolioItem title={item} url={'google.com'}/>;
         })
     }
 
@@ -20,7 +20,6 @@ export default class PortfolioContainer extends Component {
             <div>
                 <h2>Portfolio Items Go Here...</h2>
                 <h2>A component can only return one item. The parent div of this Htag is the one item.</h2>
-                <PortfolioItem />
                 {this.portfolioItems()}
             </div>
         )
