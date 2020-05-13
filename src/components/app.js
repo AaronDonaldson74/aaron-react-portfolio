80fe63182be05001a9d313ad0fd8b1a07603b5fb
AaronDonaldson74 <Dev.Aaron.Donaldson@gmail.com>
2020-05-13 15:48:26 -0600

    intro to react components

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import moment from "moment";
+
+import PortfolioContainer from "./portfolio-container";
 
 export default class App extends Component {
   render() {
@@ -7,6 +9,7 @@ export default class App extends Component {
       <div className='app'>
         <h1>Aaron Donaldson React Portfolio</h1>
         <h2>Change this through src/components/app.js</h2>
+        <PortfolioContainer />
         <div>
           {moment().format('MMMM Do YYYY, h:mm:ss a')}
         </div>
---
+++ src/components/portfolio-container.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class PortfolioContainer extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Portfolio Items Go Here...</h2>
+                <h2>A component can only return one item. The parent div of this Htag is the one item.</h2>
+            </div>
+        )
+    }
+}
