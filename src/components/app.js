8cc60c843d862368abb2d6fc8160b5a56dc15a37
AaronDonaldson74 <Dev.Aaron.Donaldson@gmail.com>
2020-05-13 11:37:04 -0600

    added moment

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import moment from "moment";
 
 export default class App extends Component {
   render() {
@@ -6,6 +7,9 @@ export default class App extends Component {
       <div className='app'>
         <h1>Aaron Donaldson React Portfolio</h1>
         <h2>Change this through src/components/app.js</h2>
+        <div>
+          {moment().format('MMMM Do YYYY, h:mm:ss a')}
+        </div>
       </div>
     );
   }
