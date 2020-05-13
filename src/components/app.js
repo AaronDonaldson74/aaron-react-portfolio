aca49d7b0dc6a509287b28842c713461b1fb5b4b
AaronDonaldson74 <Dev.Aaron.Donaldson@gmail.com>
2020-05-13 10:19:02 -0600

    initial commit

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Aaron Donaldson React Portfolio</h1>
+        <h2>Change this through src/components/app.js</h2>
+      </div>
+    );
+  }
+}
