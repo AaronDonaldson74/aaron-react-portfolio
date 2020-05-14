57488f6004c6424eef2bafde5cdf8fd8b64327dc
AaronDonaldson74 <Dev.Aaron.Donaldson@gmail.com>
2020-05-14 10:11:53 -0600

    Introduction to Working with State in React

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -4,21 +4,27 @@ import PortfolioItem from "./portfolio-item";
 export default class PortfolioContainer extends Component {
     constructor() {
         super();
-        console.log("Portfolio container has rendered");
+
+        this.state = {
+            pageTitle: "Welcome to my portfolio",
+            data: [
+                {title: "Quip"},
+                {title: "Eventbright"},
+                {title: "Ministry Safe"}
+            ]
+        };
     }
 
     portfolioItems() {
-        const data = ["Quip", "Eventbright", "Ministry Safe"];
-
-        return data.map(item => {
-            return <PortfolioItem title={item} url={'google.com'}/>;
+        return this.state.data.map(item => {
+            return <PortfolioItem title={item.title} url={'google.com'}/>;
         })
     }
 
     render() {
         return (
             <div>
-                <h2>Portfolio Items Go Here...</h2>
+                <h2>{this.state.pageTitle}</h2>
                 <h2>A component can only return one item. The parent div of this Htag is the one item.</h2>
                 {this.portfolioItems()}
             </div>
