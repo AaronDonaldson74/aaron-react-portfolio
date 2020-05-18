0efa7afbf53f9d541bac5323287794d137346524
AaronDonaldson74 <Dev.Aaron.Donaldson@gmail.com>
2020-05-18 16:16:00 -0600

    Organizing Portfolio API Query into the Portfolio Container Class

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
+import axios from 'axios';
+
 import PortfolioItem from "./portfolio-item";
+
 
 export default class PortfolioContainer extends Component {
     constructor() {
@@ -15,6 +18,8 @@ export default class PortfolioContainer extends Component {
             ]
         };
         this.handleFilter = this.handleFilter.bind(this);
+        this.getPortfolioItems = this.getPortfolioItems.bind(this);
+
     }
 
     handleFilter(filter) {
@@ -25,6 +30,17 @@ export default class PortfolioContainer extends Component {
         })
     }
 
+    getPortfolioItems() {
+        axios.get('https://aarondonaldson.devcamp.space/portfolio/portfolio_items')
+      .then(response => {
+        // handle success
+        console.log("response data",response);
+      })
+      .catch(error => {
+        // handle error
+        console.log(error);
+      })
+      }
     portfolioItems() {
         return this.state.data.map(item => {
             return <PortfolioItem title={item.title} url={'google.com'} slug={item.slug}/>;
@@ -36,12 +52,13 @@ handlePageTitleUpdate() {
         pageTitle: "Something Else"
     });
 }
-    
-    render() {
-        if (this.state.isLoading) {
-            return <div>Loading...</div>;
-        }
 
+render() {
+    if (this.state.isLoading) {
+        return <div>Loading...</div>;
+    }
+    
+    this.getPortfolioItems();
         return (
             <div>
                 <h2>{this.state.pageTitle}</h2>
