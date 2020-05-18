80f92bcc15fb65e1d6c98a8cfbf60ab38e3f9451
AaronDonaldson74 <Dev.Aaron.Donaldson@gmail.com>
2020-05-18 16:39:41 -0600

    How and Why to Use the Key Prop in React

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -38,9 +38,8 @@ export default class PortfolioContainer extends Component {
       }
     portfolioItems() {
         return this.state.data.map(item => {
-            console.log("item data", item);
-            return <PortfolioItem title={item.name} url={'google.com'} slug={item.id}/>;
-        })
+            return <PortfolioItem key={item.id} title={item.name} url={'google.com'} slug={item.id}/>;
+        });
     }
 
 handlePageTitleUpdate() {
