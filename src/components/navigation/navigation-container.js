cb079c0849c80b1b6d5e18ebac78288275e5e119
AaronDonaldson74 <Dev.Aaron.Donaldson@gmail.com>
2020-05-19 16:26:22 -0600

    Building the Navigation Link Wrapper Styles

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -11,18 +11,26 @@ export default class NavigationContainer extends Component {
         return (
             <div className="nav-wrapper">
             <div className="left-side">
+            <div className="nav-link-wrapper">
             <NavLink exact to="/">
                 Home
             </NavLink>
+            </div>
+            <div className="nav-link-wrapper">
             <NavLink to="/about-me">
                 About
             </NavLink>
+            </div>
+            <div className="nav-link-wrapper">
             <NavLink to="/contact-me">
                 Contact
             </NavLink>
+            </div>
+            <div className="nav-link-wrapper">
             <NavLink to="/blog">
                 Blog
             </NavLink>
+            </div>
                 {false ? <button>Add Blog</button> : null }
             </div>
             <div className="right-side">
