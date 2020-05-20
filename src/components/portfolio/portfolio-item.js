5697b2c674cf514d771ea1e6d6e30aa4d444be32
AaronDonaldson74 <Dev.Aaron.Donaldson@gmail.com>
2020-05-20 14:22:11 -0600

    Updating Styles with React Event Listeners

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -1,23 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 
-export default function(props) {
-    const { id, description, thumb_image_url, logo_url } = props.item
-    return (
-        <div className="portfolio-item-wrapper">
-            <div
-                className="portfolio-img-background"
-                style={{
-                    backgroundImage: "url(" + thumb_image_url + ")"
-                }}
-            />
-            
-            <div className="img-text-wrapper">
-                <div className="logo-wrapper">
-                    <img src={logo_url} />
-                    <div className="subtitle">{description}</div>
+export default class PortfolioItem extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            portfolioItemClass: ""
+        };
+    }
+
+    handleMouseEnter() {
+        this.setState({portfolioItemClass: 'image-blur '})
+    }
+
+    handleMouseLeave() {
+        this.setState({portfolioItemClass: ""});
+
+    }
+
+    render() {
+        const { id, description, thumb_image_url, logo_url } = this.props.item
+        return (
+            <div className="portfolio-item-wrapper"
+            onMouseEnter={() => this.handleMouseEnter()}
+            onMouseLeave={() => this.handleMouseLeave()}
+            >
+                <div
+                    className={"portfolio-img-background " + this.state.portfolioItemClass}
+                    style={{
+                        backgroundImage: "url(" + thumb_image_url + ")"
+                    }}
+                />
+                
+                <div className="img-text-wrapper">
+                    <div className="logo-wrapper">
+                        <img src={logo_url} />
+                        <div className="subtitle">{description}</div>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }       
 }
