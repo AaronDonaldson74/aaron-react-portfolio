8762b8994cf5c66e055a94c33d8cd209a7b00d71
AaronDonaldson74 <Dev.Aaron.Donaldson@gmail.com>
2020-05-21 16:17:08 -0600

    How to Convert a Class Component into a Functional Component in React

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,42 +1,39 @@
-import React,  { Component } from 'react';
+import React from 'react';
 import { NavLink } from "react-router-dom"
 
 
-export default class NavigationContainer extends Component {
-    constructor () {
-    super();
-
-    }
-    render () {
+const NavigationComponent = (props) => {
         return (
             <div className="nav-wrapper">
-            <div className="left-side">
-            <div className="nav-link-wrapper">
-            <NavLink exact to="/" activeClassName="nav-link-active">
-                Home
-            </NavLink>
-            </div>
-            <div className="nav-link-wrapper">
-            <NavLink to="/about-me" activeClassName="nav-link-active">
-                About
-            </NavLink>
-            </div>
-            <div className="nav-link-wrapper">
-            <NavLink to="/contact-me" activeClassName="nav-link-active">
-                Contact
-            </NavLink>
-            </div>
-            <div className="nav-link-wrapper">
-            <NavLink to="/blog" activeClassName="nav-link-active">
-                Blog
-            </NavLink>
-            </div>
-                {false ? <button>Add Blog</button> : null }
-            </div>
-            <div className="right-side">
-                Aaron Donaldson
-            </div>
+                <div className="left-side">
+                    <div className="nav-link-wrapper">
+                        <NavLink exact to="/" activeClassName="nav-link-active">
+                            Home
+                        </NavLink>
+                    </div>
+                    <div className="nav-link-wrapper">
+                        <NavLink to="/about-me" activeClassName="nav-link-active">
+                            About
+                        </NavLink>
+                    </div>
+                    <div className="nav-link-wrapper">
+                        <NavLink to="/contact-me" activeClassName="nav-link-active">
+                            Contact
+                        </NavLink>
+                    </div>
+                    <div className="nav-link-wrapper">
+                        <NavLink to="/blog" activeClassName="nav-link-active">
+                            Blog
+                        </NavLink>
+                    </div>
+                        {false ? <button>Add Blog</button> : null }
+                </div>
+                <div className="right-side">
+                    Aaron Donaldson
+                </div>
             </div>
         )
     }
-}
+
+    export default NavigationComponent;
+
