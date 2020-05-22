78842506929122af3f4cdd1705e557e508086fcc
AaronDonaldson74 <Dev.Aaron.Donaldson@gmail.com>
2020-05-22 15:45:07 -0600

    Building the Initial Portfolio Form Elements

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -1,11 +1,67 @@
 import React, { Component } from 'react';
 
 export default class PortfolioForm extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: "",
+            description: "",
+            category: "",
+            position: "",
+            url: "",
+            thumb_image: "",
+            banner_image: "",
+            logo: ""
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(event) {
+        console.log("handlechange", event)
+    }
+    
     render() {
         return (
             <div>
                 <h1>PortfolioForm</h1>
-                <h1>PortfolioForm</h1>
+
+                <form>
+                    <div>
+                        <input 
+                        type="text" 
+                        name="url" 
+                        placeholder="Portfolio Item Name" 
+                        value={this.state.url} 
+                        onChange={this.handleChange}
+                        />
+
+                    </div>
+                    <div>
+                        <input 
+                        type="text" 
+                        name="category" 
+                        placeholder="category" 
+                        value={this.state.category} 
+                        onChange={this.handleChange}
+                        />
+
+                    </div>
+                    <div>
+                        <input 
+                        type="text" 
+                        name="description" 
+                        placeholder="description" 
+                        value={this.state.description} 
+                        onChange={this.handleChange}
+                        />
+
+                    </div>
+                    <div>
+                        <button type="submit">Save</button>
+                    </div>
+
+                </form>
             </div>
         )
     }
