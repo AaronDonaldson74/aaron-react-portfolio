7008983cdff0da2926fbaac97cb5af749f31d6d5
AaronDonaldson74 <Dev.Aaron.Donaldson@gmail.com>
2020-05-22 14:39:13 -0600

    Building the Portfolio Sidebar List Component

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+import PortfolioSidebarList from "../portfolio/portfolio-sidebar-list";
+
 export default class PortfolioManager extends Component {
     constructor() {
         super();
@@ -34,8 +36,7 @@ export default class PortfolioManager extends Component {
                     <h1>Portfolio form</h1>
                 </div>
                 <div className="right-column">
-                    <h1>Portfolio sidebar</h1>
-                    {this.portfolio_items}
+                    <PortfolioSidebarList data={this.state.portfolioItems} />
                 </div>
             </div>
         )
