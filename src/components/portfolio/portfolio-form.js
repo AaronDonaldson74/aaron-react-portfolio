b2e182d487ad20421618857d881774d5966996d4
AaronDonaldson74 <Dev.Aaron.Donaldson@gmail.com>
2020-05-27 15:36:28 -0600

    Making Dynamic Axios API Queries in React\

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -18,7 +18,10 @@ export default class PortfolioForm extends Component {
             url: "",
             thumb_image: "",
             banner_image: "",
-            logo: ""
+            logo: "",
+            editMode: false,
+            apiUrl: "https://aarondonaldson.devcamp.space/portfolio/portfolio_items",
+            apiAction: 'post'
         }
 
         this.handleChange = this.handleChange.bind(this)
@@ -57,6 +60,9 @@ export default class PortfolioForm extends Component {
                 category: category || "Enterprise",
                 position: position || "",
                 url: url || "",
+                editMode: true,
+                apiUrl: `https://aarondonaldson.devcamp.space/portfolio/portfolio_items/${id}`,
+                apiAction: 'patch'
             })
         }
     }
@@ -123,11 +129,12 @@ export default class PortfolioForm extends Component {
     }
 
     handleSubmit(event) {
-        axios.post(
-            "https://aarondonaldson.devcamp.space/portfolio/portfolio_items",
-            this.buildForm(), 
-            { withCredentials: true }
-            )
+        axios({
+            method: this.state.apiAction,
+            url: this.state.apiUrl,
+            data: this.buildForm(),
+            withCredentials: true 
+            })
             .then(response => {
                 this.props.handleSuccessfulFormSubmission(response.data.portfolio_item)
                 
