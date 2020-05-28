56aedb772b591253697cd8d908c4915ec42d7790
AaronDonaldson74 <Dev.Aaron.Donaldson@gmail.com>
2020-05-28 16:20:08 -0600

    Rendering the Blog Details to the Screen

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -17,6 +17,9 @@ export default class BlogDetail extends Component {
         axios.get( `https://aarondonaldson.devcamp.space/portfolio/portfolio_blogs/${this.state.currentId}`
         ).then(response => {
             console.log("response", response);
+            this.setState({
+                blogItem: response.data.portfolio_blog
+            })
         })
         .catch(error => {
             console.log("getBlogItem error", error)
@@ -26,12 +29,22 @@ export default class BlogDetail extends Component {
     componentDidMount() {
         this.getBlogItem();
     }
-    
+
     render() {
-        console.log("currentID", this.state.currentId);
+        const {
+            title,
+            content,
+            featured_image_url,
+            blog_status
+        } =  this.state.blogItem
+
         return (
         <div>
-            <h1>Blog detail</h1>
+            <h1>{title}</h1>
+            <img src={featured_image_url} />
+            <div>
+                {content}
+            </div>
         </div>
         );
     }
