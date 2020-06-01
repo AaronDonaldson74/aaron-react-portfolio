9d3bb7c84db3cbfbffda43e992e2cf7f7d9c1a98
AaronDonaldson74 <Dev.Aaron.Donaldson@gmail.com>
2020-06-01 15:51:41 -0600

    Retrieving HTML Content from Draft JS Component

--- src/components/forms/rich-text-editor.js
+++ src/components/forms/rich-text-editor.js
@@ -4,7 +4,6 @@ import { Editor } from "react-draft-wysiwyg";
 import draftToHtml from "draftjs-to-html";
 import htmlToDraft from "html-to-draftjs";
 
-
 export default class RichTextEditor extends Component {
     constructor(props) {
         super(props);
@@ -12,8 +11,16 @@ export default class RichTextEditor extends Component {
         this.state = {
             editorState: EditorState.createEmpty()
         }
+        this.onEditorStateChange = this.onEditorStateChange.bind(this)
     }
 
+    onEditorStateChange(editorState) {
+        this.setState({ editorState }, 
+            this.props.handleRichTextEditorChange(
+                draftToHtml(convertToRaw(this.state.editorState.getCurrentContent()))
+            )
+        )
+    }
 
     render () {
         return (
@@ -22,6 +29,7 @@ export default class RichTextEditor extends Component {
                     editorState={this.state.editorState}
                     wrapperClassName="demo-wrapper"
                     editorClassName="demo-editor"
+                    onEditorStateChange={this.onEditorStateChange}
                 />
             </div>
         );
