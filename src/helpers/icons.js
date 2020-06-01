62abefee7eba51400cb17779e49c199f4c49668d
AaronDonaldson74 <Dev.Aaron.Donaldson@gmail.com>
2020-06-01 14:18:01 -0600

    Building a Dedicated Icon Helper File in React

---
+++ src/helpers/icons.js
@@ -0,0 +1,8 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faTrash, faSignOutAlt, faEdit, faSpinner, faPlusCircle } from "@fortawesome/free-solid-svg-icons";
+
+const Icons = () => {
+return library.add(faTrash, faSignOutAlt, faEdit, faSpinner, faPlusCircle);
+}
+
+export default Icons;
