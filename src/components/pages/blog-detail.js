a75d236a95cdc349190fdef977fbcb4d55560321
AaronDonaldson74 <Dev.Aaron.Donaldson@gmail.com>
2020-06-02 14:14:02 -0600

    Passing the Blog Title and Status to the Form to Edit

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -48,7 +48,7 @@ export default class BlogDetail extends Component {
 
         const contentManager = () => {
             if (this.state.editMode) {
-                return <BlogForm/>
+                return <BlogForm editMode={this.state.editMode} blog={this.state.blogItem}/>
             } else {
         return (
             <div className="content-container">
