dc0abc6a41ebeeedf5fff11242817eaa386d5835
AaronDonaldson74 <Dev.Aaron.Donaldson@gmail.com>
2020-06-03 10:28:48 -0600

    final commit

--- src/helpers/icons.js
+++ src/helpers/icons.js
@@ -1,8 +1,27 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faTrash, faSignOutAlt, faEdit, faSpinner, faPlusCircle, faPhone, faEnvelope, faMapMarkedAlt, faLock } from "@fortawesome/free-solid-svg-icons";
+import { 
+    faTrash, 
+    faSignOutAlt, 
+    faEdit, 
+    faSpinner, 
+    faPlusCircle, 
+    faPhone, 
+    faEnvelope, 
+    faMapMarkedAlt, 
+    faLock 
+} from "@fortawesome/free-solid-svg-icons";
 
 const Icons = () => {
-return library.add(faTrash, faSignOutAlt, faEdit, faSpinner, faPlusCircle, faPhone, faEnvelope, faMapMarkedAlt, faLock);
+return library.add(
+    faTrash, 
+    faSignOutAlt, 
+    faEdit, 
+    faSpinner, 
+    faPlusCircle, 
+    faPhone, 
+    faEnvelope, 
+    faMapMarkedAlt, 
+    faLock);
 }
 
 export default Icons;
