e26cee8f86201fe9a1e6b2328cea0b1f86150d4d
AaronDonaldson74 <Dev.Aaron.Donaldson@gmail.com>
2020-06-03 10:10:49 -0600

    Enabling the Ability to Delete Blog Posts

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -5,6 +5,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import BlogItem from "../blog/blog-item";
 import BlogModal from "../modals/blog-modal"
 
+
 class Blog extends Component {
     constructor() {
         super();
@@ -23,8 +24,25 @@ class Blog extends Component {
         this.handleNewBlogClick=this.handleNewBlogClick.bind(this)
         this.handleModalClose = this.handleModalClose.bind(this)
         this.handleSuccessfulNewBlogSubmission = this.handleSuccessfulNewBlogSubmission.bind(this)
+        this.handleDeleteClick = this.handleDeleteClick.bind(this)
     }
 
+    handleDeleteClick(blog) {
+        axios.delete( 
+            `https://api.devcamp.space/portfolio/portfolio_blogs/${blog.id}`,
+             { withCredentials : true})
+            .then(response => {
+                this.setState({
+                    blogItems: this.state.blogItems.filter(blogItem => {
+                        return blog.id !== blogItem.id
+                    })
+                })
+                return response.data
+            })
+            .catch(error => {
+                console.log("delete blog error", error);
+            })
+    }
     handleSuccessfulNewBlogSubmission(blog) {
         this.setState({
             blogModalIsOpen: false,
@@ -86,7 +104,18 @@ class Blog extends Component {
 
     render() {
         const blogRecords = this.state.blogItems.map(blogItem => {
-            return <BlogItem key={blogItem.id} blogItem={blogItem} />
+            if (this.props.loggedInStatus === "LOGGED_IN") {
+                return (
+                    <div key={blogItem.id} className="admin-blog-wrapper">
+                        <BlogItem blogItem={blogItem} />
+                        <a onClick={() => this.handleDeleteClick(blogItem)}>
+                            <FontAwesomeIcon icon="trash"/>
+                        </a>
+                    </div>
+                )
+            }else{
+                return <BlogItem key={blogItem.id} blogItem={blogItem} />
+            }
         });
       return (
         <div className="blog-container">
